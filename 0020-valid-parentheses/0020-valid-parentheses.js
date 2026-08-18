ee93b5e6a594cebb62345ccc275a01df4a32d21f
heena <heenaanotra@gmail.com>
2026-08-18 11:50:51 +0530

    Time: 3 ms (70.33%), Space: 54.5 MB (88.35%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -3,36 +3,27 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
-    let stack = [];
+    let stack = []
 
     let pairs = {
-        ')': '(',
-        ']': '[',
-        '}': '{'
+        ')' : '(',
+        ']' : '[',
+        '}' : '{'
     };
 
-    for (let i = 0; i < s.length; i++) {
-        let char = s[i];
+    for (let i = 0; i <s.length; i ++){
+        let char = s[i]
 
-        // opening bracket
-        if (!(char in pairs)) {
-            stack.push(char);
-        }
 
-        // closing bracket
-        else {
-            if (
-                stack.length === 0 ||
-                stack[stack.length - 1] !== pairs[char]
-            ) {
-                return false;
+        if (!(char in pairs)){
+            stack.push(char)
+        }else {
+            if(stack.length===0 ||stack[stack.length -1] !== pairs[char]){
+                return false 
             }
-
-            stack.pop();
+            stack.pop()
         }
     }
-
     return stack.length === 0;
 
 };
