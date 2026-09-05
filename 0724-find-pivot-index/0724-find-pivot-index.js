e1fa790edb7fd1ec3363c3169b73649f916f1820
heena <heenaanotra@gmail.com>
2026-09-05 17:27:54 +0530

    Time: 2 ms (67.02%), Space: 57.2 MB (65.08%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var pivotIndex = function (nums) {
+    let total = 0;
+    let leftSum = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        total += nums[i]
+    }
+
+    for (let i = 0; i < nums.length; i++) {
+
+        let rightSum = total - leftSum - nums[i];
+
+        if (leftSum === rightSum) {
+            return i;
+        }
+        leftSum += nums[i];
+    }
+    return -1
+};
