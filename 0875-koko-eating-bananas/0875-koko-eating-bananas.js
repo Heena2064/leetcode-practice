9031309d9331de46cda59cf1b9d06e8decd1445f
heena <heenaanotra@gmail.com>
2026-09-12 10:28:31 +0530

    Time: 8 ms (76.07%), Space: 57.1 MB (84.03%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} piles
+ * @param {number} h
+ * @return {number}
+ */
+var minEatingSpeed = function (piles, h) {
+    let left = 1;
+    let right = Math.max(...piles);
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        let totalhrs = 0;
+
+        for(let i = 0; i< piles.length ; i ++){
+            totalhrs += Math.ceil(piles[i]/mid)
+        }
+
+        if (totalhrs <= h) {
+
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+    return left;
+};
