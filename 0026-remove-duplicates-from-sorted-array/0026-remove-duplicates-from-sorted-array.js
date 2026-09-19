90908dee990e0dff5096a72bf432b1ced91b9656
heena <heenaanotra@gmail.com>
2026-09-19 11:22:32 +0530

    Time: 1 ms (48.98%), Space: 56.8 MB (84.25%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -2,14 +2,14 @@
  * @param {number[]} nums
  * @return {number}
  */
-var removeDuplicates = function(nums) {
+var removeDuplicates = function (nums) {
     let i = 0;
-     
-    for (j = 1; j <nums.length; j ++){
-        if(nums[i]!==nums[j]){
+
+    for (let j = 1; j < nums.length; j++) {
+        if (nums[i] !== nums[j]) {
+            nums[i + 1] = nums[j]
             i++;
-            nums[i]=nums[j]
         }
     }
-    return i +1 ;
+    return [i + 1];
 };
