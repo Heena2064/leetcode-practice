18194b21be0977300ea6b7a663fa26dbedf72120
heena <heenaanotra@gmail.com>
2026-09-22 10:41:45 +0530

    Time: 2 ms (65.93%), Space: 58 MB (94.01%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.js
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.js
@@ -3,19 +3,20 @@
  * @return {number}
  */
 var findMaxConsecutiveOnes = function (nums) {
-    let count = 0;
     let max = 0;
+    let count = 0;
 
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] === 1) {
+    for(let i =0; i < nums.length; i++){
+        if(nums[i]===1){
             count++;
-        } else {
-            count = 0
 
+            if (count >  max){
+                max = count;
+            }
+        }else{
+            count = 0;
         }
-        if (count > max) {
-            max = count;
-        }
+        
     }
     return max;
 };
